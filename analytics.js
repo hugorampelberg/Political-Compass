176da01c902d09dcba16cad78baaf1fd9a4e761f
hugorampelberg <hugorampelberg1977@gmail.com>
2026-08-12 15:45:46 +0200

    Charger les ajustements de La France Humaniste

--- analytics.js
+++ analytics.js
@@ -3,6 +3,7 @@ import './data/ps-overrides.js';
 import './data/renaissance-overrides.js';
 import './data/modem-overrides.js';
 import './data/horizons-overrides.js';
+import './data/france-humaniste-overrides.js';
 import { inject } from 'https://esm.sh/@vercel/analytics@2.0.1';
 
 inject();
