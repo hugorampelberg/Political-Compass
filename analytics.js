b6dd4e48a6f1b998e550ddbe09076124077fe941
hugorampelberg <hugorampelberg1977@gmail.com>
2026-08-12 15:11:28 +0200

    Charger les overrides d’Horizons

--- analytics.js
+++ analytics.js
@@ -2,6 +2,7 @@ import './data/ecologistes-overrides.js';
 import './data/ps-overrides.js';
 import './data/renaissance-overrides.js';
 import './data/modem-overrides.js';
+import './data/horizons-overrides.js';
 import { inject } from 'https://esm.sh/@vercel/analytics@2.0.1';
 
 inject();
