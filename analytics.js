16fdaff5626808ff6cc8f2957c122dc81dd73350
hugorampelberg <hugorampelberg1977@gmail.com>
2026-08-12 13:33:24 +0200

    Charger les overrides de Renaissance

--- analytics.js
+++ analytics.js
@@ -1,5 +1,6 @@
 import './data/ecologistes-overrides.js';
 import './data/ps-overrides.js';
+import './data/renaissance-overrides.js';
 import { inject } from 'https://esm.sh/@vercel/analytics@2.0.1';
 
 inject();
