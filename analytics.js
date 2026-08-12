2554d5ec29407430bea79b02c204dec9ff1ddda5
hugorampelberg <hugorampelberg1977@gmail.com>
2026-08-12 14:14:19 +0200

    Charger les overrides du MoDem

--- analytics.js
+++ analytics.js
@@ -1,6 +1,7 @@
 import './data/ecologistes-overrides.js';
 import './data/ps-overrides.js';
 import './data/renaissance-overrides.js';
+import './data/modem-overrides.js';
 import { inject } from 'https://esm.sh/@vercel/analytics@2.0.1';
 
 inject();
