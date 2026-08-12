0f9f079002cc01484867f82975926c969d541318
hugorampelberg <hugorampelberg1977@gmail.com>
2026-08-12 23:36:17 +0200

    Load final Q75 calibration after all data overrides

--- data/allemagne-overrides.js
+++ data/allemagne-overrides.js
@@ -98,7 +98,9 @@ if (allemagneGovernment) {
   allemagneGovernment.axisScores = scores;
 }
 
-// Dernière couche gouvernementale : chargée synchroniquement avant app.js.
+// Dernières couches synchrones : d'abord les ajustements gouvernementaux généraux,
+// puis la calibration Q75 commune aux partis et gouvernements avant app.js.
 if (typeof document !== 'undefined' && document.readyState === 'loading') {
   document.write('<script src="/data/government-final-overrides.js"></scr' + 'ipt>');
+  document.write('<script src="/data/q75-overrides.js"></scr' + 'ipt>');
 }
